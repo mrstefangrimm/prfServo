4f4629ca33b10f1ab5cc29cecdf9b2a5ec735743
= <=>
2018-06-23 09:05:01 +0200

    Updated comments

--- prfServo.h
+++ prfServo.h
@@ -9,8 +9,8 @@
  * This Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
+ * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the prfServo Library.  If not, see
  * <http://www.gnu.org/licenses/>.
